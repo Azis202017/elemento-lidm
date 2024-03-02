478d10eb46de314527f619723edd594224e18ef4
Abdul Azis Al Ayubbi <azisa6980@gmail.com>
2024-03-02 22:31:51 +0700

    feat: heading reusable widgets

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -17,22 +17,137 @@ class HomeView extends GetView<HomeController> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            RichText(
-              text: const TextSpan(
-                text: '"Chemistry is fascinating"',
-                
-                children: [
-                  TextSpan(
-                    text: "puzzle."
+            const Heading(),
+            Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(
+                        12,
+                      ),
+                      color: Colors.white,
+                    ),
+                    height: 110,
                   ),
-                  TextSpan(
-                    text: "Events experiments sparks excitement."
-                  ),
-                ]
-              ),
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 16,
+                      ),
+                      child: SizedBox(
+                        width: 106,
+                        height: 110,
+                        child: Image.asset(
+                          'assets/images/mascot.png',
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SizedBox(
+                            height: 16,
+                          ),
+                          const Text(
+                            "Jadilah Ilmuwan!",
+                            style: TextStyle(
+                              fontFamily: "Gilroy",
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          const Text(
+                            "Ayo kita bereksperimen!",
+                            style: TextStyle(
+                              color: Color(
+                                0xff90A8BF,
+                              ),
+                              fontSize: 12,
+                            ),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(
+                              right: 34,
+                            ),
+                            width: double.infinity,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              child: SizedBox(
+                                width: double.infinity,
+                                child: Center(
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Image.asset(
+                                        width: 18,
+                                        height: 18,
+                                        'assets/images/camera.png',
+                                      ),
+                                      const SizedBox(
+                                        width: 9,
+                                      ),
+                                      const Text(
+                                        "Magic Card",
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class Heading extends StatelessWidget {
+  const Heading({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: const TextSpan(
+        text: '"Kimia itu ibarat',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+        children: [
+          TextSpan(
+            text: " puzzle",
+            style: TextStyle(
+              color: Color(
+                0xffFF51A2,
+              ),
+            ),
+          ),
+          TextSpan(
+            text:
+                'yang mengasyikkan. Tiap eksperimen akan selalu menyenangkan."',
+          ),
+        ],
       ),
     );
   }
